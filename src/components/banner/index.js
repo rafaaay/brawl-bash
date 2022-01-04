50af0cd4a2e114e9605515a249f5f405e50a6d7f
Rafay <abdul.rafay@shopdev.co>
2022-01-05 01:57:59 +0500

    feat: banner cta added

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -9,7 +9,20 @@ function Banner() {
     <section className="banner">
       <div className="container">
         <Menu />
+
         <img src={images.logo} alt="logo" className="banner__logo" />
+
+        <div className="banner__cta">
+          <div className="banner__cta__container">
+            <h2 className="banner__cta__heading">Farmer</h2>
+            <div className="banner__cta__body">
+              <div className="image">
+                <img src={images.discord} alt="logo" />
+              </div>
+              <p>Join our discord now</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,7 @@
 const images = {
   logo: require("./logo.png"),
+  bg1: require("./BG-01.jpg"),
+  discord: require("./discord.png"),
 };
 
 export default images;
